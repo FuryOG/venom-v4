4888baf79e7d12b8088ab272906b6e17cba2826b
FuryOG <somaguptainfo@gmail.com>
2024-08-31 20:17:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7245863648:AAEMA28XoLunz6lGzkmKfLB903yos4DsB6g")
 
   #Enter Your telegram username here without @
 OWNER_USERNAME=("venomXcrazy")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("5016323749")
 
 
 
